fe43664e72cdf0a497492dbb62a8f05925ad5002
annabala <aniabala92@gmail.com>
2021-02-11 21:08:13 +0100

    cpt update

--- cms/wp-content/themes/sup-up/includes/page-config/custom-post/create-custom.php
+++ cms/wp-content/themes/sup-up/includes/page-config/custom-post/create-custom.php
@@ -1,26 +1,6 @@
 <?php
 // Our custom post type function
 function sup_create_posttype() {
-  register_post_type('events',
-    array(
-      'labels'        => array(
-      'name'          => __( 'Eventy' ),
-      'singular_name' => __( 'event' ),
-    ),
-    'public'      => true,
-    'show_ui'             => true,
-    'show_in_menu'        => true,
-    'show_in_nav_menus'   => true,
-    'show_in_admin_bar'   => true,
-    'has_archive' => true,
-    'menu_icon'   => 'dashicons-smiley',
-    'supports' => array( 'title', 'editor'),
-    'rewrite' => array(
-      'slug' => 'events'
-    ),
-    )
-  );
-
   register_post_type( 'blog',
     array(
       'labels' => array(
@@ -34,35 +14,6 @@ function sup_create_posttype() {
       'rewrite' => array('slug' => 'blog'),
     )
   );
-
-  register_post_type( 'fun-park',
-    array(
-      'labels' => array(
-      'name' => __( 'Fun Park' ),
-      'singular_name' => __( 'atrakcja' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'menu_icon'   => 'dashicons-palmtree',
-      'supports' => array( 'title', 'editor'),
-      'rewrite' => array('slug' => 'fun-park'),
-    )
-  );
-
-  register_post_type( 'vr',
-    array(
-      'labels' => array(
-      'name' => __( 'SUP VR' ),
-      'singular_name' => __( 'atrakcja' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'menu_icon'   => 'dashicons-games',
-      'supports' => array( 'title', 'editor'),
-      'rewrite' => array('slug' => 'vr'),
-    )
-  );
-
 
   register_post_type('multimedia',
     array(
